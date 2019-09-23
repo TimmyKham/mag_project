b325e8c75d899a1119e5f033df7a652d7f04bbad
TimmyKham <tkhamsithideth@gmail.com>
2019-09-23 19:34:27 +0200

    UniqueEntity title

--- src/Entity/Magazines.php
+++ src/Entity/Magazines.php
@@ -4,10 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
-use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MagazinesRepository")
+ * @UniqueEntity("title")
  */
 class Magazines
 {
